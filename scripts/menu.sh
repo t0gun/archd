582c64d7dbad073e94cbc89eb61636afcbf7ea6e
tobiloba ogundiyan <apprentice_py@icloud.com>
2025-07-24 13:29:31 -0400

    added wget and curl

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")")"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 SCRIPTS=(
   base.sh fonts.sh iwd.sh docker.sh setup-firewall.sh hyprland.sh asdcontrol.sh appl-fn-keys.sh
